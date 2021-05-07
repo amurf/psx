30bd7a3abdab3cfa99f2bf47b7e9c0da788da957
ash <ash@strategicdata.com.au>
2021-05-07 11:42:16 +1000

    convert from table to nice cover images

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,37 @@
 <template>
-  <div class="container">
-
-    <div v-if="entries.items" class='w-3/12'>
+  <div>
+    <div v-if="entries.items" class='m-auto nav'>
       {{ entries.skip }}-{{ entries.skip + entries.items.length }} of {{ entries.total }}
       <br>
       <button @click="prev" class="p-1 rounded shadow bg-blue-100" v-if="entries.skip != 0">prev</button>
       <button @click="next" class="p-1 rounded shadow bg-blue-100" v-if="entries.skip + entries.items.length != entries.total">next</button>
     </div>
 
-    <table class='w-full'>
-      <thead>
+    <div class="grid">
+
+
+      <div v-for="entry in entries.items" class='games'>
+        <img v-if="entry.fields.cover" :src="entries.includes.Asset.find(item => item.sys.id == entry.fields.cover.sys.id).fields.file.url" />
+        <div v-else class='placeholder-image border'>No cover</div>
+        {{ entry.fields.serialNumber }}: {{ entry.fields.officialTitle }}
+      </div>
+
+      <!--
+        <table class='w-full'>
+        <thead>
         <th class='w-4/12 text-left'>Serial</th>
         <th class='text-left'>Name</th>
-      </thead>
-      <tbody>
+        </thead>
+        <tbody>
         <tr v-for="entry in entries.items">
-          <td class='text-left'>{{ entry.fields.serialNumber }}</td>
-          <td class='text-left'>{{ entry.fields.officialTitle }}</td>
+        <td class='text-left'>{{ entry.fields.serialNumber }}</td>
+        <td class='text-left'>{{ entry.fields.officialTitle }}</td>
         </tr>
-      </tbody>
-    </table>
+        </tbody>
+        </table>
+      -->
 
+    </div>
   </div>
 </template>
 
@@ -58,27 +69,40 @@ export default {
 .container {
 @apply min-h-screen flex justify-center items-center text-center mx-auto mt-2;
 }
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+ */
+.grid {
+  grid-template-columns: auto auto auto auto;
+  width: 70%;
+  margin: auto;
+  grid-gap: 20px;
+}
+
+.games {
+  width: 250px;
+}
+
+.nav {
+  margin-top: 1.5em;
+  margin-bottom: 2em;
   text-align: center;
+}
+
+.games img, .games .placeholder-image {
+  width: 250px;
+  height: 250px;
 }
 
 .title {
   font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  'Quicksand',
+  'Source Sans Pro',
+  -apple-system,
+  BlinkMacSystemFont,
+  'Segoe UI',
+  Roboto,
+  'Helvetica Neue',
+  Arial,
+  sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
