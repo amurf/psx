fd6d48312db9c3bcae8339471a6bf8c28fd26ed4
Ashley Murphy <ash.d.murphy@gmail.com>
2021-08-27 13:39:34 +1000

    Add server config for local development.

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,11 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  server: {
+    port: 5555,
+    host: '0.0.0.0'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'psx',
