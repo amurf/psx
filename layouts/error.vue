ac197520dc65c7e1398a734845be29f8b6f66ee8
ash <ash@strategicdata.com.au>
2021-08-29 10:47:03 +1000

    Add missing template tags.

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,8 @@
-<div v-if="error.statusCode === 404">
-  <h1>Page not found</h1>
-</div>
-<div v-else>
-  <h1>Something unexpected went wrong :(</h1>
-</div>
-
+<template>
+  <div v-if="error.statusCode === 404">
+    <h1>Page not found</h1>
+  </div>
+  <div v-else>
+    <h1>Something unexpected went wrong :(</h1>
+  </div>
+</template>
