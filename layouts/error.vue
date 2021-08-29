a098570aae5f31ebab84f619633ed1603605e43b
ash <ash@strategicdata.com.au>
2021-08-29 11:08:25 +1000

    Just display a simple error for now
    will return to this.

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,8 +1,3 @@
 <template>
-  <div v-if="error.statusCode === 404">
-    <h1>Page not found</h1>
-  </div>
-  <div v-else>
-    <h1>Something unexpected went wrong :(</h1>
-  </div>
+  <h1>Something unexpected went wrong :(</h1>
 </template>
